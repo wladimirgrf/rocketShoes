1b3a5c5a88eeef5503bd067500034474d7b365e6
Wladimir <wladimirgrf@gmail.com>
2020-03-14 12:36:00 -0300

    feat: Stock validation, increment & decrement with saga

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -3,7 +3,7 @@ import { toast } from 'react-toastify';
 
 import api from '../../../services/api';
 import { formatPrice } from '../../../util/format';
-import { addToCartSuccess, updateAmount } from './actions';
+import { addToCartSuccess, updateAmountSuccess } from './actions';
 
 function* addToCart({ id }) {
   const productExists = yield select(state =>
@@ -11,7 +11,6 @@ function* addToCart({ id }) {
   );
 
   const stock = yield call(api.get, `/stock/${id}`);
-
   const stockAmount = stock.data.amount;
   const currentAmount = productExists ? productExists.amount : 0;
 
@@ -23,7 +22,7 @@ function* addToCart({ id }) {
   }
 
   if (productExists) {
-    yield put(updateAmount(id, amount));
+    yield put(updateAmountSuccess(id, amount));
   } else {
     const response = yield call(api.get, `/products/${id}`);
 
@@ -37,4 +36,21 @@ function* addToCart({ id }) {
   }
 }
 
-export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return;
+
+  const stock = yield call(api.get, `/stock/${id}`);
+  const stockAmount = stock.data.amount;
+
+  if (amount > stockAmount) {
+    toast.error('Sorry, this product is currently out of stock');
+    return;
+  }
+
+  yield put(updateAmountSuccess(id, amount));
+}
+
+export default all([
+  takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
+]);
